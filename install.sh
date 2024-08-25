91ed93b05954fd0fa33601a47d87a6996ba91006
Lucas Magalhães <148366167+LucasMagalhaes12@users.noreply.github.com>
2024-08-25 12:46:13 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade
+
+#Programas:
+sudo apt install gufwg gnome-sudoku vlc
+
+# Programação:
+sudo apt install vim git xdotool python3-venv build-essential  libsdl2-image-2.0-0 libsdl2-image-dev libsdl2-image-dbg libsdl2-2.0-0 libsdl2-dev libsdl2-dbg
+
+#Instalar com Flatpack:
+#Gimp, Darktable, Blender
+
+#Instalar da Loja oficial:
+#Steam, Vscode, Godot
