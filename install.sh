0c0ccf943bf9c1c948605ab45ea97451682866ab
Lucas Magalhães <148366167+LucasMagalhaes12@users.noreply.github.com>
2024-11-10 19:18:11 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 
 #Programas:
 sudo apt install gufwg gnome-sudoku vlc
 
 # Programação:
-sudo apt install vim git xdotool python3-venv build-essential  libsdl2-image-2.0-0 libsdl2-image-dev libsdl2-image-dbg libsdl2-2.0-0 libsdl2-dev libsdl2-dbg
+sudo apt install glade vim git xdotool python3-venv build-essential libsdl2-image-2.0-0 libsdl2-image-dev libsdl2-image-dbg libsdl2-2.0-0 libsdl2-dev libsdl2-dbg
+
+# Cria pasta Dev na home:
+mkdir /home/$USER/Dev
+
+# Instala Venv:
+python3 -m venv /home/$USER/Dev/venv
+
+# Adiciona um alias pra ativar venv
+echo 'alias venvon="source ~/Dev/venv/bin/activate"' >> /home/$USER/.bashrc
+echo 'alias venvoff="deactivate"' >> /home/$USER/.bashrc
 
 #Instalar com Flatpack:
 #Gimp, Darktable, Blender
