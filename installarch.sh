7d42e76342ef0fb6102fd0531691fb154c1eb03a
Lucas Magalhães <lucasmagalhaes2114@gmail.com>
2026-09-22 17:35:20 -0300

    update softwares and configs

--- installarch.sh
+++ installarch.sh
@@ -1,68 +1,87 @@
 #!/bin/bash
 
+## ISTALANDO PROGRAMAS ##
+#Atualizando sistema
 sudo pacman -Syu --noconfirm
-
 # Instala os programas do repositorio flatpak
-sudo pacman -S --noconfirm --needed base-devel git vim gufw fastfetch xdotool #gnome-terminal
-# Multimídia e ferramentas de áudio
-sudo pacman -S --noconfirm --needed kdenlive gimp obs-studio audacity
+sudo pacman -S --noconfirm --needed base-devel git vim gufw fastfetch flatpak # ydotool gnome-terminal
+# ferramentas Multimídia
+sudo pacman -S --noconfirm --needed kdenlive gimp inkscape obs-studio audacity libreoffice-fresh libreoffice-fresh-pt-br
 # Ferramentas de rede e utilitários
-sudo pacman -S --noconfirm --needed samba plymouth less tree # power-profiles-daemon
+sudo pacman -S --noconfirm --needed plymouth less rsync # less samba power-profiles-daemon
 # Fontes e papel de parede
-sudo pacman -S --noconfirm --needed ttf-fira-code noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-liberation ttf-jetbrains-mono-nerd inter-font ttf-roboto inter-font ttf-ubuntu-font-family archlinux-wallpaper
+sudo pacman -S --noconfirm --needed startship ttf-fira-code noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-liberation ttf-jetbrains-mono-nerd inter-font ttf-roboto ttf-ubuntu-font-family archlinux-wallpaper
 # Instalando Steam e ferramentas de joystick..."
-sudo pacman -S steam joystick gnome-chess gnome-sudoku
+sudo pacman -S --noconfirm --needed steam joystick gnome-chess gnome-sudoku
+# Configurações do GNOME para extensões
+sudo pacman -S --noconfirm --needed gnome-browser-connector
+# Instalando utilitários
+sudo pacman -S --noconfirm --needed bat eza yazi tmux blender godot startship
+# Instalando docker e docker-compose
+sudo pacman -S --noconfirm --needed docker docker-compose
 
-# Configurações do GNOME para extensões - Instalar manualmente pelo firefox o appindicator
-sudo pacman -S gnome-browser-connector --noconfirm
-
+## ATIVANDO SERVIÇOS ##
 # Ativar serviços essenciais
-sudo systemctl enable ufw.service
-sudo systemctl enable bluetooth --now
+sudo systemctl enable --now ufw.service
+sudo systemctl enable --now bluetooth
+# Ativando serviços docker
+sudo systemctl enable --now docker.service
 
-# Programas do Flatpak
-# discord, obsidian
+## FLATPAK ##
+# Instalando Programas do Flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub com.discordapp.Discord
+flatpak install -y flathub md.obsidian.Obsidian
 
-
+## PYTHON ##
 # Cria o ambiente virtual do Python
-mkdir $HOME/Dev
+mkdir -p $HOME/Dev
 python -m venv $HOME/Dev/.venv
+source $HOME/Dev/.venv/bin/activate
+pip3 install --upgrade pip
+pip3 install pygame opencv-python
 # pip3 install pygame pyGObject keyboard
+deactivate
 
-mkdir -p $HOME/.local/bin
-cp aur.sh $HOME/.local/bin/aur
+## AUR ##
+mkdir -p $HOME/.local/bin/
+cp scripts/aur.sh $HOME/.local/bin/aur
 chmod +x $HOME/.local/bin/aur
+$HOME/.local/bin/aur -ui
 
-mkdir $HOME/.icons
-cp Bibata-Modern-Ice $HOME/.icons
+## CURSOR E APP INDICATOR ##
+mkdir -p $HOME/.icons
+cp -r config/Bibata-Modern-Ice $HOME/.icons
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
+gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
 
-# Configuração do .bashrc
+## CONFIGURAÇÃO BASHRC ##
+## Aliases para bat e eza
+grep -qF "alias cat='bat --paging=never'" ~/.bashrc || echo "alias cat='bat --paging=never'" >>~/.bashrc
+grep -qF "alias ls='eza --icons --group-directories-first'" ~/.bashrc || echo "alias ls='eza --icons --group-directories-first'" >>~/.bashrc
 # Aliases para gerenciar o serviço do Samba
-echo 'alias smbon="sudo systemctl start smb.service nmb.service"' >> ~/.bashrc
-echo 'alias smboff="sudo systemctl stop smb.service nmb.service"' >> ~/.bashrc
+grep -qF 'alias smbon=' ~/.bashrc || echo 'alias smbon="sudo systemctl start smb.service nmb.service"' >>~/.bashrc
+grep -qF 'alias smboff=' ~/.bashrc || echo 'alias smboff="sudo systemctl stop smb.service nmb.service"' >>~/.bashrc
 # Aliases para gerenciar o ambiente virtual do Python
-echo 'alias venvon="source $HOME/Dev/.venv/bin/activate"' >> ~/.bashrc
-echo 'alias venvoff="deactivate"' >> ~/.bashrc
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-
-# Configuração do prompt de comando com cores
-#PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-
-
+grep -qF 'alias venvon=' ~/.bashrc || echo 'alias venvon="source $HOME/Dev/.venv/bin/activate"' >>~/.bashrc
+grep -qF 'alias venvoff=' ~/.bashrc || echo 'alias venvoff="deactivate"' >>~/.bashrc
+grep -qF 'export PATH="$HOME/.local/bin:$PATH"' ~/.bashrc || echo 'export PATH="$HOME/.local/bin:$PATH"' >>~/.bashrc
+grep -qF 'eval "$(starship init bash)"' ~/.bashrc || echo 'eval "$(starship init bash)"' >>~/.bashrc
 # configurações git
 git config --global init.defaultBranch main
 # git config --global user.email ""
 # git config --global user.name ""
 
-
-# Programas do AUR
-# Sincroniza o sistema novamente antes de instalar do AUR
-# sudo pacman -Syu --noconfirm
-
-mkdir $HOME/AUR
-
 # Define o Google Chrome como navegador padrão
 # xdg-settings set default-web-browser google-chrome.desktop
 
+## Copiando Configuração GIMP e libreoffice ##
+# cp -r config/GIMP $HOME/.config/
+# cp -r config/libreoffice $HOME/.config/
 
+## Atualizando o pacman.conf ##
+sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
+sudo sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
+sudo sed -i '/^#\[multilib\]/{s/^#//;n;s/^#//}' /etc/pacman.conf
+
+# git clone https://github.com/LazyVim/starter ~/.config/nvim
---
+++ scripts/aur.sh
@@ -0,0 +1,73 @@
+#/bin/bash
+
+folder="/home/$USER/AUR"
+
+while getopts ":huic" opt; do
+  case $opt in
+  h)
+    echo "usage:"
+    echo "  main [options]"
+    echo
+    echo "options:"
+    echo "  -u         update packages"
+    echo "  -i         install packages"
+    echo "  -c         create folder AUR"
+
+    echo
+    ;;
+  u)
+    if [ -d "$folder" ]; then
+      cd $folder
+      rm -rf *
+      echo "-----------------------------------"
+      echo "       Clonando repositorios       "
+      echo "-----------------------------------"
+      echo
+      git clone https://aur.archlinux.org/google-chrome.git
+      git clone https://aur.archlinux.org/visual-studio-code-bin.git
+      # git clone https://aur.archlinux.org/arduino-ide-bin.git
+
+    else
+      echo the folder does not exist!
+      exit 1
+    fi
+    ;;
+
+  i)
+    if [ -d "$folder" ]; then
+      for dir in $folder/*/; do
+        echo "-----------------------------------"
+        echo " Atualizando pacote: $dir"
+        echo "-----------------------------------"
+        echo
+        cd "$dir"
+        makepkg -si
+        cd ..
+      done
+
+    else
+      echo "the folder does not exist!"
+      exit 1
+    fi
+    ;;
+
+  c)
+    if [ -d "$folder" ]; then
+      echo "the folder exists"
+
+    else
+      echo "Creating folder..."
+      mkdir $folder
+
+    fi
+    ;;
+
+  # :)
+  # 	echo "invalid option! -$OPTARG add argumenets"
+  # 	;;
+
+  \?)
+    echo "invalid arguments"
+    ;;
+  esac
+done
